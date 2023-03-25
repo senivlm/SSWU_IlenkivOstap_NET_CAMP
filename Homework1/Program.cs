19ab5c57206a257059de604ebd0ebcaca814e94d
Ostap <ostapilienkiv@gmail.com>
2023-03-25 13:46:59 +0200

    Task 3 completed

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,14 +1,27 @@
-﻿namespace Homework1
+﻿using System;
+
+namespace Homework1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Task1.GenerateSpiralMatrix(3, 4, Task1.StartPoint.BottomLeft, Task1.Direction.ToRight);
-            Console.WriteLine("Pres any key to continue.");
-            Console.ReadKey();
-            Task2 task2 = new Task2();
-            task2.GetIndexesAndLenght();
+            //Task1.GenerateSpiralMatrix(3, 4, Task1.StartPoint.BottomLeft, Task1.Direction.ToRight);
+            //Console.WriteLine("Pres any key to continue.");
+            //Console.ReadKey();
+            //Task2 task2 = new Task2();
+            //task2.GetIndexesAndLenght();
+            Task3 task3 = new Task3();
+            task3.GenerateVoids();
+            task3.FindThroughLinearHole();
+
+
+
+
+
+
+
+            
         }
     }
 }
